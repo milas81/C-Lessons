1a6dc09625c7f9c87352c8a977713f216c580a98
liudmila <milas81@mail.ru>
2022-09-29 19:00:26 +0300

    Condition if_else 2

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write a username: ");
+String username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Hip, Hip!!! It's MASHA!!!");
+}
+else
+{
+Console.Write("Hello, ");
+Console.WriteLine(username);
+}
